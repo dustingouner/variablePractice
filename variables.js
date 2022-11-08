1c47256c9de5e2f5fd549a45597fe018ba075ad9
Dustin Gouner <dustingouner20@gmail.com>
2022-11-08 15:39:17 -0700

    Add changes to variables

--- variables.js
+++ variables.js
@@ -1,6 +1,9 @@
-var name = "Dustin"
+var firstName = "Dustin"
+firstName = "John"
 var city = "Denver"
+city = "Boulder"
 var homeTown = "Baton Rouge"
+homeTown = "Boston"
 
 var age = 37
 var kids = 1
@@ -8,3 +11,10 @@ var pets = 1
 
 var isMarried = true
 var enjoysThisCourse = true
+
+console.log(isMarried)
+console.log(firstName)
+console.log(age)
+console.log(enjoysThisCourse)
+
+
